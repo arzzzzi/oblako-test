23c8575845c810e75e8fdbdac7e3166d82c76f7f
arzzzzi <kantyshevmagomed@gmail.com>
2024-03-17 17:02:21 +0300

    Готово, осталось обновление после сохранения

--- ProductsMVC/wwwroot/js/site.js
+++ ProductsMVC/wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
+    var PlaceHolderElement = $('#PlaceHolderHere');
 
-// Write your JavaScript code.
+    $('button[data-toggle="ajax-modal"], button[data-toggle="ajax-modal-edit"]').click(function (event) {
+        var url = $(this).data('url');
+        var decodedUrl = decodeURIComponent(url);
+        $.get(decodedUrl).done(function (data) {
+            PlaceHolderElement.html(data);
+            PlaceHolderElement.find('.modal').modal('show');
+        });
+    });
+
+    PlaceHolderElement.on('click', 'button[data-save="modal"]', function (event) {
+        var form = $(this).parents('.modal').find('form');
+        var actionUrl = form.attr('action');
+        var url = /Products/ + actionUrl;
+        var sendData = form.serialize();
+
+        $(this).prop('disabled', true);
+
+        $.post(url, sendData).done(function (data) {
+            PlaceHolderElement.find('.modal').modal('hide');
+        });
+    });
+
+    PlaceHolderElement.on('click', '#saveEditButton', function (event) {
+        var form = $(this).parents('.modal').find('form');
+        var actionUrl = form.attr('action');
+        var sendData = form.serialize();
+
+        $(this).prop('disabled', true);
+
+        $.post(actionUrl, sendData).done(function (data) {
+            PlaceHolderElement.find('.modal').modal('hide');
+        });
+    });
+
+    PlaceHolderElement.on('click', '[data-dismiss="modal"]', function (event) {
+        PlaceHolderElement.find('.modal').modal('hide');
+    });
+});
